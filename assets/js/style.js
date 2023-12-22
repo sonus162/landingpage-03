7aae496d65a4ca5544bf2521fe746e1e35d94c0d
HoangSonNguyen <hoangson16298@gmail.com>
2023-12-22 11:17:34 +0700

    dev testimonial

--- assets/js/style.js
+++ assets/js/style.js
@@ -21,3 +21,26 @@ valueDisplays.forEach((valueDisplay) => {
         }
     });
 });
+
+let currentImage = 1;
+let countSlider = document.querySelectorAll(".testimonial__avatar").length;
+
+function prevImage() {
+    currentImage = currentImage > 2 ? currentImage - 1 : 6;
+    console.log(currentImage);
+    showImage("avatar", 1, currentImage);
+}
+
+function nextImage() {
+    currentImage = currentImage < countSlider ? currentImage + 1 : 2;
+    showImage("avatar", 1, currentImage);
+}
+
+function showImage(id_name, start, end) {
+    id_name_start = id_name + start;
+    id_name_end = id_name + end;
+    const imgStart = document.getElementById(id_name_start).getAttribute("src");
+    const imgEnd = document.getElementById(id_name_end).getAttribute("src");
+    document.getElementById(id_name_start).src = imgEnd;
+    document.getElementById(id_name_end).src = imgStart;
+}
